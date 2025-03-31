826d35c2806f2e0732a5794f365d3b940c847ed1
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-03-31 19:59:35 +0530

    Update syd.py

--- mfinder/plugins/syd.py
+++ mfinder/plugins/syd.py
@@ -1,11 +1,12 @@
 
 from pyrogram import Client, filters, enums
-
+from mfinder.db.join_req import JoinReqs
 join_db = JoinReqs
 
 
 @Client.on_chat_join_request((filters.group | filters.channel))
 async def auto_approve(client, message: ChatJoinRequest):
+    AUTH_CHANNEL = await get_channel()
     if message.chat.id == AUTH_CHANNEL:
       ap_user_id = message.from_user.id
       first_name = message.from_user.first_name
